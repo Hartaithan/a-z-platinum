3580d10236dded9cddf149678b8db580f6713cb8
Hartaithan <hartaithan@gmail.com>
2025-03-22 16:13:47 +0900

    clean up and prettier, eslint setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,9 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import prettier from "eslint-config-prettier";
+import { dirname } from "path";
+import tseslint from "typescript-eslint";
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +14,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettier,
+  {
+    ignores: [".next/**", "node_modules/**", "dist/**"],
+    rules: {
+      "no-console": ["error", { allow: ["info", "error", "warn"] }],
+    },
+  },
 ];
 
 export default eslintConfig;

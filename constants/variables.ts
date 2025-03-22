1c197a1ad3b8878b9de32f54b854ef9edea69397
Hartaithan <hartaithan@gmail.com>
2025-03-22 15:46:19 +0900

    init fresh next app

--- constants/variables.ts
+++
@@ -1,3 +0,0 @@
-export const URL = process.env.NEXT_PUBLIC_URL ?? "";
-export const API_URL = process.env.NEXT_PUBLIC_API_URL ?? "";
-export const SECRET = process.env.NEXT_PUBLIC_SECRET ?? "";
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,6 @@
-import { FlatCompat } from "@eslint/eslintrc";
-import js from "@eslint/js";
-import prettier from "eslint-config-prettier";
 import { dirname } from "path";
-import tseslint from "typescript-eslint";
 import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -14,10 +11,6 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  js.configs.recommended,
-  ...tseslint.configs.recommended,
-  prettier,
-  { ignores: [".next/**", "node_modules/**", "dist/**"] },
 ];
 
 export default eslintConfig;

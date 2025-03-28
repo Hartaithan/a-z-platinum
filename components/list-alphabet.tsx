65f1f1fadce5ca82a77fd276adb6219c46722e74
Hartaithan <hartaithan@gmail.com>
2025-03-28 22:12:49 +0900

    display item and counts in list theme

--- components/list-alphabet.tsx
+++ components/list-alphabet.tsx
@@ -1,16 +1,39 @@
+import { useData } from "@/providers/data";
 import { FC } from "react";
 
-const letters = "abcdefghijklmnopqrstuvwxyz#0".split("");
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ#0".split("");
+
+interface LetterProps {
+  letter: string;
+}
+
+const Letter: FC<LetterProps> = (props) => {
+  const { letter } = props;
+  const { getItemKeys, getItem } = useData();
+  const { items, hasItems, count } = getItemKeys({ letter });
+  const firstKey = hasItems ? items[0] : null;
+  const item = getItem(firstKey);
+  return (
+    <div className="flex">
+      <p className="w-5 text-center capitalize">{letter}</p>
+      <p className="mr-1">-</p>
+      {hasItems ? (
+        <>
+          <p>{item?.title}</p>
+          <p>&nbsp;- {count} plats</p>
+        </>
+      ) : (
+        <p>not found</p>
+      )}
+    </div>
+  );
+};
 
 const ListAlphabet: FC = () => {
   return (
     <div className="flex flex-col gap-1">
       {letters.map((letter) => (
-        <div key={letter} className="flex">
-          <p className="w-5 text-center capitalize">{letter}</p>
-          <p className="mr-1">-</p>
-          <p>not found</p>
-        </div>
+        <Letter key={letter} letter={letter} />
       ))}
     </div>
   );

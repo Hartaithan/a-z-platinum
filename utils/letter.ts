756c7fabba1180382dc080cf2ec58ecac2c5f75d
Hartaithan <hartaithan@gmail.com>
2025-03-29 14:29:31 +0900

    remove 'The' from title regardless of case

--- utils/letter.ts
+++ utils/letter.ts
@@ -2,9 +2,10 @@ const specialChar = /[^\p{L}\p{N}]/u;
 const asianChar = /[\u3040-\u30FF\u4E00-\u9FFF\u3400-\u4DBF]/;
 
 export const recognizeLetter = (title: string) => {
-  const letter = title.replace("The ", "")[0].toUpperCase();
+  let letter = title[0];
+  if (title.toLowerCase().startsWith("the ")) letter = title[4];
   if (!isNaN(Number(letter))) return "0";
   if (specialChar.test(letter)) return "#";
   if (asianChar.test(letter)) return "賞";
-  return letter;
+  return letter.toUpperCase();
 };

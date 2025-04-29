8124158993fc8274080f83103b98474a2261522d
Hartaithan <hartaithan@gmail.com>
2025-04-29 21:00:52 +0900

    refactor use progress hook

--- hooks/use-progress.ts
+++ hooks/use-progress.ts
@@ -2,9 +2,17 @@
 
 import { completed, letters } from "@/constants/letters";
 import { useData } from "@/providers/data";
+import { toFixed } from "@/utils/number";
 import { useEffect, useState } from "react";
 
-const defaultProgress = { count: 0, value: 0, left: 0 };
+const defaultProgress = { count: 0, value: 0, left: 0, label: "0%" };
+
+export const getProgress = (current: number, total: number) => {
+  const progress = (current / total) * 100;
+  const notValid = progress < 0 || isNaN(progress) || !isFinite(progress);
+  if (notValid) return { value: 0, label: "0%" };
+  return { value: progress, label: `${toFixed(progress)}%` };
+};
 
 export const useProgress = () => {
   const { getItemKeys } = useData();
@@ -13,9 +21,9 @@ export const useProgress = () => {
 
   useEffect(() => {
     const count = document.getElementsByClassName(completed)?.length || 0;
-    const value = (count * 100) / total;
+    const { value, label } = getProgress(count, total);
     const left = total - count;
-    setProgress({ count, value, left });
+    setProgress({ count, value, left, label });
   }, [getItemKeys, total]);
 
   return { ...progress, total };

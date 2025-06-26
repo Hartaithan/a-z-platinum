4368272f22e15632d0ac2201390ff7ee7e880cc7
Hartaithan <hartaithan@gmail.com>
2025-06-26 16:21:37 +0900

    use sheet component instead of drawer

--- components/settings-drawer.tsx
+++ components/settings-drawer.tsx
@@ -11,16 +11,15 @@ import { dataKeys, dataLabels, themes, themesLabels } from "@/constants/app";
 import { useSettings } from "@/providers/settings";
 import { useTheme } from "@/providers/theme";
 import {
-  Drawer,
-  DrawerClose,
-  DrawerContent,
-  DrawerDescription,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerTrigger,
-} from "@/ui/drawer";
-import { Settings, X } from "lucide-react";
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/ui/sheet";
+import { Settings } from "lucide-react";
 import { FC, memo } from "react";
 
 const Content: FC = () => {
@@ -29,53 +28,48 @@ const Content: FC = () => {
     useSettings();
 
   return (
-    <>
-      <div className="flex flex-col space-y-4 p-4 pt-2">
-        <SettingsSelect
-          id="theme"
-          label="Theme"
-          placeholder="Select theme"
-          options={themes}
-          labels={themesLabels}
-          value={theme}
-          onValueChange={changeTheme}
-        />
-        <SettingsSelect
-          id="data"
-          label="Data"
-          placeholder="Select data type"
-          options={dataKeys}
-          labels={dataLabels}
-          value={settings.data}
-          onValueChange={setDataSetting}>
-          <DataHint />
-        </SettingsSelect>
-        <SettingsSelect
-          id="difficulty"
-          label="Difficulty"
-          placeholder="Select difficulty"
-          options={difficultyKeys}
-          labels={difficultyLabels}
-          value={settings.difficulty}
-          onValueChange={setDifficultySetting}>
-          <DifficultyHint />
-        </SettingsSelect>
-        <SettingsSwitch
-          id="hide"
-          label="Hide Profile"
-          checked={settings.hide}
-          onCheckedChange={setHideSetting}>
-          <p className="text-xs font-normal text-neutral-500">
-            enable this option to <b>blur your profile</b>, making the personal
-            information visually obscured
-          </p>
-        </SettingsSwitch>
-        <FeaturedReset />
-      </div>
-      <DrawerFooter>
-        <SettingsReset />
-      </DrawerFooter>
-    </>
+    <div className="flex flex-1 flex-col space-y-4 p-4 pt-2">
+      <SettingsSelect
+        id="theme"
+        label="Theme"
+        placeholder="Select theme"
+        options={themes}
+        labels={themesLabels}
+        value={theme}
+        onValueChange={changeTheme}
+      />
+      <SettingsSelect
+        id="data"
+        label="Data"
+        placeholder="Select data type"
+        options={dataKeys}
+        labels={dataLabels}
+        value={settings.data}
+        onValueChange={setDataSetting}>
+        <DataHint />
+      </SettingsSelect>
+      <SettingsSelect
+        id="difficulty"
+        label="Difficulty"
+        placeholder="Select difficulty"
+        options={difficultyKeys}
+        labels={difficultyLabels}
+        value={settings.difficulty}
+        onValueChange={setDifficultySetting}>
+        <DifficultyHint />
+      </SettingsSelect>
+      <SettingsSwitch
+        id="hide"
+        label="Hide Profile"
+        checked={settings.hide}
+        onCheckedChange={setHideSetting}>
+        <p className="text-xs font-normal text-neutral-500">
+          enable this option to <b>blur your profile</b>, making the personal
+          information visually obscured
+        </p>
+      </SettingsSwitch>
+      <FeaturedReset />
+    </div>
   );
 };
 
@@ -83,26 +77,26 @@ const SettingsContent = memo(Content);
 
 const Component: FC = () => {
   return (
-    <Drawer direction="right">
-      <DrawerTrigger>
+    <Sheet>
+      <SheetTrigger>
         <Settings />
-      </DrawerTrigger>
-      <DrawerContent>
-        <DrawerHeader className="flex-row">
-          <DrawerTitle>Settings</DrawerTitle>
-          <DrawerDescription className="hidden">
+      </SheetTrigger>
+      <SheetContent className="w-full gap-0 overflow-auto">
+        <SheetHeader className="flex-row">
+          <SheetTitle>Settings</SheetTitle>
+          <SheetDescription className="hidden">
             Settings drawer
-          </DrawerDescription>
-          <DrawerClose className="ml-auto">
-            <X />
-          </DrawerClose>
-        </DrawerHeader>
+          </SheetDescription>
+        </SheetHeader>
         <SettingsContent />
-      </DrawerContent>
-    </Drawer>
+        <SheetFooter>
+          <SettingsReset />
+        </SheetFooter>
+      </SheetContent>
+    </Sheet>
   );
 };
 
-const SettingsDrawer = memo(Component);
+const SettingsSheet = memo(Component);
 
-export default SettingsDrawer;
+export default SettingsSheet;

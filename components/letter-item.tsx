69ce023390b99a32e46033263c728d7f3d990d51
Hartaithan <hartaithan@gmail.com>
2025-07-14 21:14:14 +0900

    use virtualized list in letter modal

--- components/letter-item.tsx
+++ components/letter-item.tsx
@@ -2,16 +2,23 @@ import GameContent from "@/components/game-content";
 import GameProgress from "@/components/game-progress";
 import GameTrophy from "@/components/game-trophy";
 import LetterFeatured from "@/components/letter-featured";
-import { FC, memo } from "react";
+import { cn } from "@/utils/styles";
+import { ComponentPropsWithoutRef, FC, memo } from "react";
 
-interface ItemProps {
+interface ItemProps extends ComponentPropsWithoutRef<"div"> {
   item: string;
+  last: boolean;
 }
 
 const LetterItem: FC<ItemProps> = (props) => {
-  const { item } = props;
+  const { item, last, ...rest } = props;
   return (
-    <div className="relative flex flex-col rounded-md border p-3">
+    <div
+      className={cn(
+        "relative mb-3 flex flex-col rounded-md border p-3",
+        last && "mb-0",
+      )}
+      {...rest}>
       <LetterFeatured item={item} />
       <GameContent item={item} />
       <GameProgress item={item} />
--- components/letter-items.tsx
+++ components/letter-items.tsx
@@ -1,6 +1,7 @@
 import LetterItem from "@/components/letter-item";
 import { useLetter } from "@/providers/letter";
 import { FC, memo } from "react";
+import { VList } from "virtua";
 
 const EmptyList: FC = () => (
   <div className="flex items-center justify-center">
@@ -12,11 +13,13 @@ const LetterItems: FC = () => {
   const { items } = useLetter();
   if (!items || items.length === 0) return <EmptyList />;
   return (
-    <div className="w-full-scrollbar scrollbar-gutter flex max-h-[80vh] flex-col gap-y-3 overflow-auto">
-      {items.map((item) => (
-        <LetterItem key={item} item={item} />
+    <VList
+      className="!w-full-scrollbar scrollbar-gutter flex-col gap-y-3 overflow-auto"
+      style={{ height: "80vh" }}>
+      {items.map((item, index) => (
+        <LetterItem key={item} item={item} last={items.length === index + 1} />
       ))}
-    </div>
+    </VList>
   );
 };
 

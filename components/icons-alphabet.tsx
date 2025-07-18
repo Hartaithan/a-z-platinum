81a49d42d5df414f037def597e0095477e5c6669
Hartaithan <hartaithan@gmail.com>
2025-07-18 17:21:46 +0900

    add count and letter badge to icons theme

--- components/icons-alphabet.tsx
+++ components/icons-alphabet.tsx
@@ -18,6 +18,8 @@ const styles = {
     "relative flex flex-col items-center rounded-md aspect-square size-full shadow-md shadow-black/25 overflow-hidden",
   letter:
     "absolute top-1/2 left-1/2 text-7xl font-extrabold -translate-x-1/2 -translate-y-1/2 z-10 flex items-center justify-center rounded-full capitalize text-neutral-300",
+  badge:
+    "absolute z-10 flex items-center justify-center rounded-md border border-black/20 bg-white px-1 text-xs capitalize shadow-md",
 };
 
 const Letter: FC<LetterProps> = (props) => {
@@ -28,7 +30,7 @@ const Letter: FC<LetterProps> = (props) => {
   const { getFeatured, openLetterModal } = useFeatured();
 
   const dataKey = settings.data;
-  const { items, hasItems, status } = getItemKeys({
+  const { items, hasItems, count, status } = getItemKeys({
     dataKey,
     letter,
     year,
@@ -56,12 +58,24 @@ const Letter: FC<LetterProps> = (props) => {
         src={item?.trophy?.image_url ?? ""}
         alt={item?.title ?? "Game image"}
       />
+      <b
+        className={cn(
+          styles.badge,
+          "top-2 left-2 size-5 rounded-full px-0",
+          status,
+        )}>
+        {letter}
+      </b>
+      {hasItems && count > 1 && (
+        <b className={cn(styles.badge, "top-2 right-2", status)}>
+          {`+${count - 1}`}
+        </b>
+      )}
       <div className="absolute z-10 flex size-full flex-col justify-end bg-linear-to-b from-transparent via-transparent via-60% to-black/60 p-2">
         <p className="text-shadow line-clamp-3 text-xs leading-[normal] font-extrabold break-words text-ellipsis text-white">
           {title}
         </p>
       </div>
-      <b className={cn("hidden", status)}>{letter}</b>
     </div>
   );
 };
